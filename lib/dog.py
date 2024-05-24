9ec49b81fd14f282c1922995cacfcf9258cd044c
Abraham <abukip344@gmail.com>
2024-05-23 22:54:33 -0400

    init self

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    '''creates dog class'''
+    def __init__(self, name, breed="Mutt"):
+        '''initializing attributes'''
+        self.name = name
+        self.breed = breed
+
+
+
+your_dog = Dog("Casey")
+my_dog = Dog("Stoner", "Bosco")
+
+print(f"{your_dog.name} belongs to the breed {your_dog.breed}")
+print(f"{my_dog.name} belongs to the breed {my_dog.breed}")
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    '''creates persons with names'''
+
+    def __init__(self, name):
+        '''initialize attributes'''
+        self.name = name
+
+
+    
+person_1 = Person("Ibra")
+
+print(person_1.name)
